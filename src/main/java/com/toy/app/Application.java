91ae4da579c05030b2f5f0f7f173c14d120a40f7
rlqja910 <kkb509@naver.com>
2022-09-20 09:33:20 +0900

    배치돌아감. 대시보드진입시 멤버1개증가. 어플리케이션 실행시 id를 모두 2로체인지

--- src/main/java/com/toy/app/Application.java
+++ src/main/java/com/toy/app/Application.java
@@ -1,11 +1,13 @@
 package com.toy.app;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
--- src/main/java/com/toy/app/core/util/batch/JobConfig.java
+++ src/main/java/com/toy/app/core/util/batch/JobConfig.java
@@ -2,6 +2,7 @@ package com.toy.app.core.util.batch;
 
 import java.util.List;
 
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -20,29 +21,30 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Configuration
-public class Job {
+public class JobConfig {
 	
+	@Autowired
 	private MemberRepository memberRepository;
 	
 	//.get("myjob") : job이라는 이름의 Job을 생성하는 JobBulder 인스턴스를 반환받고자 합니다.
 	//.start(myStep) : mySt`ep이라는 메서드에서 Step이라는 인스턴스를 생성하여 반환받습니다.
 	//.build(); : 이 메소드를 통해 비로소 myJob이라는 Job이 빌드가 되면서 Job이 생성됩니다. 
 	@Bean
-	public org.springframework.batch.core.Job myJob(JobBuilderFactory jobBuilderFactory,
+	public Job myJob(JobBuilderFactory jobBuilderFactory,
 			Step mystep) {
-		return jobBuilderFactory.get("myJob") //job builder 생성됨.
-				.preventRestart()
+		return jobBuilderFactory.get("myJob2") //job builder 생성됨.
+//				.preventRestart() //job 중복실행 방지
 				.start(mystep)
 				.build();
 	}
 	
 	@Bean
 	public Step mystep(StepBuilderFactory stepBuilderFactory) {
-		return stepBuilderFactory.get("mystep")
+		return stepBuilderFactory.get("mystep2")
 				.<Member,Member> chunk(100)
 				.reader(myReader())
-				.processor(myProcessor())
-				.writer(myWriter())
+				.processor(this.myProcessor())
+				.writer(this.myWriter())
 				.build();
 	}
 	
@@ -50,7 +52,6 @@ public class Job {
 	@StepScope
 	public ListItemReader<Member> myReader(){
 		System.out.println("================myReader===================");
-		//공지사항 조회
 		List<Member> oldMember = (List<Member>) memberRepository.findAll();
 		System.out.println(oldMember.toString());
 		return new ListItemReader<>(oldMember);
